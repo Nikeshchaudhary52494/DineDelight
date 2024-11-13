235b6179f948c3e821d075765617151b9faf2445
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-11-13 23:14:29 +0530

    searchbar added

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,17 +1,27 @@
 import { db } from "@/lib/db";
-import Image from "next/image";
-import * as motion from "framer-motion/client";
-import Link from "next/link";
+import Image from "next/image"
 import RestaurantGrid from "@/components/restaurant/resturentgrid";
+import SearchBar from "@/components/home/searchBar";
 
 export default async function Home() {
-
+  const restaurants = await db.restaurant.findMany();
   return (
-    <div className="max-w-6xl p-4 mx-auto space-y-4">
-      {/* <SearchBar /> */}
-      <h2 className="text-5xl font-bold">Top resturents near you</h2>
-      <RestaurantGrid />
-      {/* <MyPagination /> */}
+    <div className="p-4 mx-auto space-y-4">
+      <div className="relative w-full justify-center items-center h-56 flex">
+
+        <SearchBar restaurants={restaurants} />
+        <Image
+          className="absolute object-cover -z-10"
+          src={"/assets/authback.jpg"}
+          alt="foodImage"
+          fill
+        />
+      </div>
+      <div className="max-w-6xl mx-auto">
+        <h2 className="text-5xl font-bold">Top resturents near you</h2>
+        <RestaurantGrid />
+        {/* <MyPagination /> */}
+      </div>
     </div>
   );
 }
