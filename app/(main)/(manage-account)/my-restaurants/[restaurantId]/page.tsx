82316a7dd6429c5c3ef67bf3e52014a0263e1b31
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-12-01 03:09:04 +0530

    bug fixed

--- app/(main)/(manage-account)/my-restaurants/[restaurantId]/page.tsx
+++ app/(main)/(manage-account)/my-restaurants/[restaurantId]/page.tsx
@@ -21,14 +21,14 @@ export default async function Page({ params }: ManageMenuProps) {
     })
 
     const menuItemAvailable = restaurant?.menu.length === 0 ? false : true;
-    const isInitial = restaurant?.tableLayout ? false : true;
+    const isLayoutSet = restaurant?.tableLayout ? true : false;
 
     return (
         <MyRestaurant
             restaurant={restaurant as Restaurant}
             menu={restaurant?.menu!}
             tablelayout={restaurant?.tableLayout!}
-            canGoLive={menuItemAvailable && isInitial}
+            canGoLive={menuItemAvailable && isLayoutSet}
         />
     )
 }
